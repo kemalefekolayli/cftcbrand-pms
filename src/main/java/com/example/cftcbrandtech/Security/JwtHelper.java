21dbed426ff76f2bdd0cab5b15f272f8e15651d5
Kemal Efe Kolaylı <efe.kolayli@sabanciuniv.edu>
2025-12-07 18:39:10 +0300

    asd

--- src/main/java/com/example/cftcbrandtech/Security/JwtHelper.java
+++ src/main/java/com/example/cftcbrandtech/Security/JwtHelper.java
@@ -6,6 +6,7 @@ import com.example.cftcbrandtech.User.Dto.UserProfileDto;
 import com.example.cftcbrandtech.User.Service.UserService;
 import com.example.cftcbrandtech.User.UserEntity;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -17,6 +18,7 @@ import java.util.Collection;
 @RequiredArgsConstructor
 public class JwtHelper {
 
+    @Lazy
     private final UserService userService;
 
     public UserProfileDto getCurrentUser() {
